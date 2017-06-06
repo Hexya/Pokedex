541df397f84f45f051099bd4f2bbd21919600a5d
loic-belaid <loicbere@gmail.com>
2017-06-06 22:44:34 +0200

    update target

--- src/Components/Body/PokeTarget.jsx
+++ src/Components/Body/PokeTarget.jsx
@@ -20,24 +20,42 @@ class PokeTarget extends React.Component {
     console.log(this.props.params.value)
     return (
       <div>
-        <Header />
+        <Link to="/"><Header /></Link>
         <div className="containTGrid">
           <div className="gridTCards">
               <div className="nextP">
                 <Link to={{ pathname: '/pokeTarget/' + data.pokemonInfo[this.props.params.value-2].id }}>
-                  <p>Prev:{data.pokemonInfo[this.props.params.value-2].name}</p>
+                  <p><i className="fa fa-angle-double-left" aria-hidden="true"></i> N°{data.pokemonInfo[this.props.params.value-2].num}</p>
                 </Link>
                 <Link to={{ pathname: '/pokeTarget/' + data.pokemonInfo[this.props.params.value].id }}>
-                  <p>Next:{data.pokemonInfo[this.props.params.value].name}</p>
+                  <p>N°{data.pokemonInfo[this.props.params.value].num} <i className="fa fa-angle-double-right" aria-hidden="true"></i></p>
                 </Link>
               </div>
-              <div className="pokeDisp">
-                <p>name:{data.pokemonInfo[this.props.params.value-1].name}</p>
+              <div className="pokeDispName">
+                <p>{data.pokemonInfo[this.props.params.value-1].name}</p>
+              </div>
+
+                <div className="pokeDispCont">
+                <div className="pokeDispImg">
+
           <img className="pokeImgTarget" src={data.pokemonInfo[this.props.params.value-1].img} alt="Pokemon"/>
-          <p>height:{data.pokemonInfo[this.props.params.value-1].height}</p>
-          <p>weight:{data.pokemonInfo[this.props.params.value-1].weight}</p>
+        </div>
+          <div className="pokeStat">
+          <div className="pokeStat1">
+          <p><span>Height:</span> {data.pokemonInfo[this.props.params.value-1].height}</p>
+          <p><span>Weight:</span> {data.pokemonInfo[this.props.params.value-1].weight}</p>
+          <p><span>Weaknesses:</span> </p><p className="PoketType">{data.pokemonInfo[this.props.params.value-1].weaknesses}</p>
+        </div>
+          <div className="pokeStat2">
+          <p><span>Number:</span> {data.pokemonInfo[this.props.params.value-1].num}</p>
+          <p><span>Type:</span> {data.pokemonInfo[this.props.params.value-1].type}</p>
           {/* <p>Evolution:{data.pokemonInfo[this.props.params.value-1].next_evolution.name}</p> */}
         </div>
+        </div>
+    </div>
+    <div className="pokeBot">
+    <img src="https://d30y9cdsu7xlg0.cloudfront.net/png/58337-200.png" alt="pokeball"></img>
+    </div>
     </div>
   </div>
 </div>
