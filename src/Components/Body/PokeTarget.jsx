8f973b1923f372165072e18d95d7bc692ff44ee8
loic-belaid <loicbere@gmail.com>
2017-06-06 20:30:47 +0200

    update target page

--- src/Components/Body/PokeTarget.jsx
+++ src/Components/Body/PokeTarget.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import data from './Data.js';
 import "./PokeTarget.min.css";
 import Header from "../Header/Header";
-// import PokeTargetCards from './PokeTargetCards.jsx'
+import { Link } from 'react-router'
 
 
 class PokeTarget extends React.Component {
@@ -23,21 +23,25 @@ class PokeTarget extends React.Component {
         <Header />
         <div className="containTGrid">
           <div className="gridTCards">
-          <div className="targetCards">
-            <p>name:{data.pokemonInfo[this.props.params.value-1].name}</p>
-            <img className="pokeImgTarget" src={data.pokemonInfo[this.props.params.value-1].img} alt="Pokemon"/>
-            <p>weight:{data.pokemonInfo[this.props.params.value-1].weight}</p>
-            <p>Evolution:{data.pokemonInfo[this.props.params.value-1].ev}</p>
-            {/* {data.pokemonInfo.map((i, index) => { //map dans array pokemon // index pour definir key diff
-              return (
-              <PokeTargetCards weight={i.weight} key={index}/>
-            )
-          })} */}
+              <div className="nextP">
+                <Link to={{ pathname: '/pokeTarget/' + data.pokemonInfo[this.props.params.value-2].id }}>
+                  <p>Prev:{data.pokemonInfo[this.props.params.value-2].name}</p>
+                </Link>
+                <Link to={{ pathname: '/pokeTarget/' + data.pokemonInfo[this.props.params.value].id }}>
+                  <p>Next:{data.pokemonInfo[this.props.params.value].name}</p>
+                </Link>
+              </div>
+              <div className="pokeDisp">
+                <p>name:{data.pokemonInfo[this.props.params.value-1].name}</p>
+          <img className="pokeImgTarget" src={data.pokemonInfo[this.props.params.value-1].img} alt="Pokemon"/>
+          <p>height:{data.pokemonInfo[this.props.params.value-1].height}</p>
+          <p>weight:{data.pokemonInfo[this.props.params.value-1].weight}</p>
+          {/* <p>Evolution:{data.pokemonInfo[this.props.params.value-1].next_evolution.name}</p> */}
         </div>
-        </div>
-      </div>
     </div>
-  )
+  </div>
+</div>
+)
 }
 }
 
